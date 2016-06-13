1fd190c2c636216a5afd747226e32b7a3f2febb6
Dalal <dalal-alzeer@hotmail.com>
2016-06-13 14:00:43 -0400

    Update testInvokerBuilder

--- test/ui/command/TestInvokerBuilder.java
+++ test/ui/command/TestInvokerBuilder.java
@@ -7,23 +7,35 @@ import environment.TestEnvironment;
 import lifeform.Human;
 import lifeform.LifeForm;
 import ui.GameDisplay;
+import junit.framework.TestCase;
+import lifeform.Human;
+import lifeform.LifeForm;
 
-public class TestInvokerBuilder
+
+public class TestInvokerBuilder extends TestCase
 {
-
-	@Test
-	public void test()
-	{
-		GameDisplay display = new GameDisplay();
-		Environment env = Environment.getWorldInstance();
-		env.setDisplay(display);
+	
+	public void test() 
+	{  
 
 		LifeForm entity1 = new Human("bob", 40, 10);
-		env.addLifeForm(0, 0, entity1);
-		Invoker in = InvokerBuilder.bulidInvoker(entity1);
 
 		// test buttons function correctly
 		TestEnvironment.resetEnvironment();
+		Invoker in=InvokerBuilder.bulidInvoker(entity1);
+		//test buttons function correctly
+		assertTrue(in.turnNorth instanceof TurnNorth);
+		assertTrue(in.turnSouth instanceof TurnSouth);
+		assertTrue(in.turnEast instanceof TurnEast);
+		assertTrue(in.turnWest instanceof TurnWest);
+		assertTrue(in.move instanceof Move);
+		assertTrue(in.reload instanceof Reload);
+		assertTrue(in.attack instanceof Attack);
+		assertTrue(in.drop instanceof Drop);
+		assertTrue(in.acquire instanceof Acquire);
+		
+		
 	}
 
+	
 }
